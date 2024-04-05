62b3449090e993d48078ed5d4410041e03f8b838
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-04-05 15:43:23 +0900

    Create beacon_connect.py

---
+++ beacon_connect.py
@@ -0,0 +1,28 @@
+from bluepy.btle import Scanner, DefaultDelegate
+import base64
+
+class BeaconDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+        self.found_beacon = False
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if not self.found_beacon:
+            for (adtype, desc, value) in dev.getScanData():
+                if value.startswith("aafe"):
+                    # 'aafe'로 시작하는 경우, 맨 앞에서부터 8글자를 삭제하고 출력 - URL 문자열 부분
+                    hex_value = value[8:]
+                
+                    # 2f(/)를 기준으로 SSID와 패스워드 분리
+                    ssid_hex, password_hex = hex_value.split("2f")
+                    
+                    # 16진수를 ASCII문자열로 변환하여 SSID와 패스워드 출력
+                    ssid = ''.join([chr(int(ssid_hex[i:i+2], 16)) for i in range(0, len(ssid_hex), 2)])
+                    password = ''.join([chr(int(password_hex[i:i+2], 16)) for i in range(0, len(password_hex), 2)])
+                    return [ssid, password]
+
+if __name__ == "__main__":
+    scanner = Scanner().withDelegate(BeaconDelegate())
+    scanner.start()
+    scanner.process(timeout=3)  # 3초 동안 스캔
+    scanner.stop()
