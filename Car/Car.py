945c5b68623ee3067b55388c68dcfc199f90047f
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-05-09 15:19:00 +0900

    send_signal start

--- Car/Car.py
+++ Car/Car.py
@@ -1,4 +1,4 @@
-from TCP_client import recv_data, recv_UDP_data, connect_TCP #UDP, TCP연결 관련
+from TCP_client import recv_data, recv_UDP_data, connect_TCP, send_signal #UDP, TCP연결 관련
 from Beacon_Network_Connect import BeaconDelegate, connect_to_wifi, scan_beacon #Network 연결 관련
 
 import socket
@@ -11,6 +11,8 @@ import time
 # UDP 데이터 받기
 threading.Thread(target=recv_UDP_data).start()
 
+# Signal
+threading.Thread(target=send_signal).start()
 
 # Beacon 스캔 계속
 threading.Thread(target=scan_beacon).start()
