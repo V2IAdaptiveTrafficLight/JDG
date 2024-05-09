0fc1c06dd7e5744cb88f22ea55d81e79b5004348
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-05-09 15:19:56 +0900

    Update TCP_client.py

--- Car/TCP_client.py
+++ Car/TCP_client.py
@@ -1,16 +1,34 @@
+import RPi.GPIO as GPIO
+import time
 import socket
 import threading
 import pickle
 import datetime
+ 
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BCM)
+
+# 버튼 핀의 입력설정 , PULL DOWN 설정 
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
 s_data = {'pass' : 0} #우회전 유무 패킷
+right = 0
 
 def recv_data(client_socket):
     while True:
         data = client_socket.recv(1024)
         r_time = datetime.datetime.now().strftime("%H:%M:%S")
         r_data = pickle.loads(data)
-        print(f"\n>> HOST의 message : {r_data} ({r_time})")
+        print(f">> HOST의 message : {r_data} ({r_time})")
+
+def send_signal():
+    while True:
+        global right
+        
+        if GPIO.input(15) == GPIO.HIGH:
+            time.sleep(0.1)
+            right = not right
+        time.sleep(0.1)
 
 #TCP 연결
 def connect_TCP(HOST, PORT):
@@ -22,15 +40,17 @@ def connect_TCP(HOST, PORT):
     print('>> Connect Server')
     
     while True:
-        global s_data
+        global s_data, right
         
-        pass_v = input("Enter value for pass : ")   #건너는 유무 판단 - 추후 버튼으로 수정
+        time.sleep(0.1)    
+        if right == True:
+            s_data['pass'] = int(right)
         
-        s_data['pass'] = pass_v
-        
-        s_time = datetime.datetime.now().strftime("%H:%M:%S")
-        data = pickle.dumps([s_data, s_time])
-        client_socket.sendall(data)
+            s_time = datetime.datetime.now().strftime("%H:%M:%S")
+            data = pickle.dumps([s_data, s_time])
+            client_socket.sendall(data)  
+        else:
+            continue
 
     client_socket.close()
 
