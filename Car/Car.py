b66beda2da485c62d9bbe83b9727d801af103944
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-06-03 16:49:46 +0900

    Update Car.py

--- Car/Car.py
+++ Car/Car.py
@@ -1,5 +1,6 @@
 from TCP_client import recv_data, recv_UDP_data, connect_TCP, send_signal #UDP, TCP연결 관련
 from Beacon_Network_Connect import BeaconDelegate, connect_to_wifi, scan_beacon #Network 연결 관련
+from moter import forward, backward, left, right, stop, move
 
 import socket
 import threading
@@ -16,3 +17,6 @@ threading.Thread(target=send_signal).start()
 
 # Beacon 스캔 계속
 threading.Thread(target=scan_beacon).start()
+
+# Move
+threading.Thread(target=move).start()
